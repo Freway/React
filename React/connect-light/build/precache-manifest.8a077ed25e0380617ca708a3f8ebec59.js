fcc4edcb8eea10fd36c323c34fd348f5fdfa8168
Freway <gui-h-m@outlook.com>
2019-11-26 17:23:26 -0300

    Upload connect-light

---
+++ React/connect-light/build/precache-manifest.8a077ed25e0380617ca708a3f8ebec59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08dc3bef9e174e787eeb151b6fb4621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b0363c846b1e2a038f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4d4712d3d260c0420c9d",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "f6b0363c846b1e2a038f",
+    "url": "/static/js/2.ad1b8517.chunk.js"
+  },
+  {
+    "revision": "4d4712d3d260c0420c9d",
+    "url": "/static/js/main.b05dbb07.chunk.js"
+  },
+  {
+    "revision": "722adcc66530fea54dcd",
+    "url": "/static/js/runtime~main.b5fa2b5f.js"
+  }
+]);
